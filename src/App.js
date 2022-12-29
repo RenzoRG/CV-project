ea32a6a441cf1a70c817292f8e6e4b0bac376265
Renzo R. Gelmetti <renzo.gelmetti@seznam.cz>
2022-12-29 16:48:19 +0100

    major edit: made different layout of the App

--- src/App.js
+++ src/App.js
@@ -1,80 +1,59 @@
 import React from 'react'
-import './styles/styles.css'
+import GeneralInfo from './components/GeneralInfo';
 
 class App extends React.Component {
-  constructor() {
-    super()
-
+  constructor(props){
+    super(props)
     this.state = {
-      form: {
-        'personal info': {
-          'first name': '',
-          'last name': '',
-          email: 'email',
-          'phone number': 'phone number',
-        },
-        education: {
-          'school name': 'school name',
-          'title': 'title',
-          graduation: 'year of graduation',
-        },
-        'work experience': {
-          'company name': 'company name',
-          'position title': 'position title',
-          'job tasks': 'job tasks',
-          'starting date': 'starting date of the job',
-          'ending date': 'last day at the job'
+          //general info
+          firstName: '',
+          lastName: '',
+          email: '',
+          number: '',
+          //education
+          title: '',
+
+          //switch from submit to edit
+          status:'edit'
         }
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleEdit = this.handleEdit.bind(this)
       }
-    }
+
+  handleChange(e) {
+    const inputName = e.target
+    this.setState({[inputName.name]: inputName.value});
   }
-render()
-{  return (
-    <div className="App">
-      <header className="header">
-        <h1>CV Application creator</h1>
-      </header>
-      <div className="main">
-        <div className="form container">
-          <form className="section generalInfo">
-            <label>Personal info:</label>
-            <input placeholder='first name'/>
-            <input placeholder='last name'/>
-            <input placeholder='email'/>
-            <input placeholder='phone number'/>
-          </form>
-          <form className="section education">
-            <label>Education info:</label>
-            <input placeholder='school name'/>
-            <input placeholder='title'/>
-            <input placeholder='year of graduation'/>
-          </form>
-          <form className="section work">
-            <label>Work experience info:</label>
-            <input placeholder='company name'/>
-            <input placeholder='position title'/>
-            <input placeholder='job tasks'/>
-            <input placeholder='starting date on the job'/>
-            <input placeholder='ending date on the job'/>
-          </form>
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({status: 'done'})
+  }
+
+  handleEdit(e) {
+    e.preventDefault();
+    this.setState({status: 'edit'})
+  }
+
+  render() {
+    const {firstName, lastName, email, number, status} = this.state
+      return (
+        <div className="App">
+          <GeneralInfo 
+            firstName={firstName}
+            lastName={lastName}
+            email={email}
+            number={number}
+            status={status}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            handleEdit={this.handleEdit}
+          />
         </div>
-        <div>
-          <section>
-            <p>first name</p>
-            <p>last name</p>
-            <p>email</p>
-            <p>phone number</p>
-          </section>
-        </div>
-      </div>        
-    </div>
-  );}
+      )
+    }
 }
 
 export default App;
-
-{/* each section should have an edit and submit button
-<h1>section with general info: name, email, phone number</h1>
-<h1>section with education info: school name, title of study date of study</h1>
-<h1>section with practical experience: company name, position title, main tasks of our jobs, date from and until when we worked at that company</h1>
-    */}
---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+class GeneralInfo extends React.Component {
+    render() {
+        const {firstName, lastName, email, number, status} = this.props
+        if ( status === 'edit') {
+          return (
+              <form onSubmit={this.props.handleSubmit}>
+                <label htmlFor='firstName'></label>
+                <input
+                  name='firstName'
+                  value={firstName}
+                  onChange={this.props.handleChange}
+                />
+                <label htmlFor='lastName'></label>
+                <input
+                  name='lastName'
+                  value={lastName}
+                  onChange={this.props.handleChange}
+                />
+                <label htmlFor='email'></label>
+                <input
+                  name='email'
+                  value={email}
+                  onChange={this.props.handleChange}
+                />
+                <label htmlFor='number'></label>
+                <input
+                  name='number'
+                  value={number}
+                  onChange={this.props.handleChange}
+                />
+                <button type='submit'>Submit</button>
+              </form>
+          )
+        }
+        return(
+          <div>
+            <h1>{firstName}</h1>
+            <h1>{lastName}</h1>
+            <h1>{email}</h1>
+            <h1>{number}</h1>
+            <button onClick={this.props.handleEdit}>Edit</button>
+          </div>
+        )
+        }
+}
+
+export default GeneralInfo
